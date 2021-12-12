3ef7779152d870f1c43f5eb9f35970e4a410ff6f
SkrynnikAndrew <exigoll@bk.ru>
2021-12-12 18:34:48 +0500

    fix: name

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
 // Скрывающееся меню в tasks
-let coll = document.getElementsByClassName('collapsible');
+let coll = document.getElementsByClassName('collapsible')
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener('click', function() {
-    this.classList.toggle('active');
-    let content = this.nextElementSibling;
+    this.classList.toggle('active')
+    let content = this.nextElementSibling
     if (content.style.maxHeight) {
-      content.style.maxHeight = null;
+      content.style.maxHeight = null
     } else {
-      content.style.maxHeight =content.scrollHeight + 'px';
+      content.style.maxHeight = content.scrollHeight + 'px'
     }
   })
 }
-let users = [
-  {id: 1, name: 'Andrew'},
-  {id: 2, name: 'John'},
-  {id: 3, name: 'Mike'},
-  ]
-
-let user = users.find(item => item.id == 1)
-console.log(user.name) // Andrew
